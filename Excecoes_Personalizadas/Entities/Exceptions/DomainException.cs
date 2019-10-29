748b93bc85eb6f59bd06ce620eae3586edccd4a2
Antonio Carlos <antoniolp_92@hotmail.com>
2019-10-28 21:29:52 -0300

    Resolucao exercicio excecoes personalizadas Curso C#

---
+++ Excecoes_Personalizadas/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Excecoes_Personalizadas.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
